7d1d6d1e019f29cce8b3123e24d3678db456ea32
Chad <cjlonghorn@gmail.com>
2024-04-01 12:55:55 -0400

    Set log_level to commandline argument

--- code/Backend/ComposeGenerator/comp_gen/__main__.py
+++ code/Backend/ComposeGenerator/comp_gen/__main__.py
@@ -1,7 +1,5 @@
 import argparse
 import logging
-logging.basicConfig(format='[%(levelname)s|%(filename)s|%(funcName)s-%(lineno)d]: %(message)s',level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
 from . import run
 
@@ -9,11 +7,18 @@ def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Generate docker compose file for 90snet")
     parser.add_argument("config_path", type=argparse.FileType('r'), help="Input configuration file")
     parser.add_argument('output_path', type=argparse.FileType('w'), help="Output docker compose file")
+    parser.add_argument("-v", "--verbose", action='store_true')
 
     return parser.parse_args()
 
 def command_run():
     args = get_args()
+
+    logging.basicConfig(
+        format='[%(levelname)s|%(filename)s|%(funcName)s-%(lineno)d]: %(message)s',
+        level=logging.DEBUG if args.verbose else logging.INFO
+    )
+
 
     print("Running Now")
     run(args.config_path, args.output_path)
