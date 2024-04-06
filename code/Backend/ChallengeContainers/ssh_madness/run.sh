12938cad08497063439ae6056c975f7064a40d76
Chad <cjlonghorn@gmail.com>
2024-04-06 00:17:21 -0400

    Will this daemon ever work

--- code/Backend/ChallengeContainers/ssh_madness/run.sh
+++ code/Backend/ChallengeContainers/ssh_madness/run.sh
@@ -8,9 +8,22 @@ popd
 
 # Now run the ssh server
 cd /home
-service ssh start -D
-#/usr/sbin/sshd -D
+service ssh start
 
-wait -n
-exit $#
+# Function to handle SIGINT signal
+sigint_handler() {
+    echo "Received SIGINT signal. Stopping SSH service..."
+    # Stop SSH service
+    service ssh stop
+    exit 0
+}
+
+# Trap SIGINT signal and call sigint_handler function
+trap sigint_handler SIGINT
+
+# Wait indefinitely with a sleep command
+echo "SSH service started. Waiting for SIGINT signal..."
+while true; do
+    sleep 3600  # Sleep for an hour before checking for signals again
+done
 
