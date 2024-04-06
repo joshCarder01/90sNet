008a454368961dd29c31d964162c5edd40a8bc54
Chad <cjlonghorn@gmail.com>
2024-04-06 00:17:50 -0400

    Use Runner Script

--- code/Backend/ChallengeContainers/ssh_madness/ssh_adversary.sh
+++ code/Backend/ChallengeContainers/ssh_madness/ssh_adversary.sh
@@ -2,45 +2,6 @@
 
 echo "Creating Background Process to Disable Competitor"
 
-# Fork into a new shell to allow for some sleeps and stuff
-del_deny() {
-    sleep 120
-    sed -i "/DenyUsers root@$1/d" /etc/ssh/sshd_config
-    service ssh restart
-}
-
-sleep 10
-
-TARGET=$(last | grep "still logged in" | awk "{printf \"/dev/%s,%s\n\", \$2, \$3}")
-if test -z "$TARGET"; then exit 0; fi
-for line in $TARGET; do
-    IFS="," read -r dev ip <<< $line
-    bash -c "echo \"Detected Out of Place IP from $ip\" > $dev"
-done
-
-
-sleep 20
-TARGET_SEC=$(last | grep "still logged in" | awk "{printf \"/dev/%s,%s\n\", \$2, \$3}")
-if test -z "$TARGET_SEC"; then exit 0; fi
-while IFS="," read -r dev ip; do 
-    if grep -q "$ip" <<< "$TARGET"; then
-        echo "UPDATING FIREWALL TO BLOCK $ip" > $dev
-        proc=$(ps -aux | ps -aux | grep --color=never -E 'sshd: [[:alnum:]]+@' | grep --color=never "$(basename $dev)" | awk '{print $2}')
-        DENY_TEXT="DenyUsers root@$ip"
-
-        # Funny
-        echo "YOU ARE BLOCKED!" > $dev
-        kill $proc
-
-        # Deny future login attempts
-        echo $DENY_TEXT >> /etc/ssh/sshd_config
-        service ssh restart
-
-        # Fork process to delete text in the future
-        (del_deny $ip) &
-    fi
-done <<< "$TARGET_SEC"
-
 # Start the daemon
 nohup bash <<< "$(dirname $0)/runner.sh" > /dev/null 2>&1 &
 
